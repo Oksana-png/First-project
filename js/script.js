8d3f5fa2b9d6aa5bbe4ad1755f6e3066040584c2
Oksana Ovchinnikova <oksana_ovchinnikova@inbox.ru>
2021-04-03 17:42:08 +0500

    Create value variebles and metods

--- js/script.js
+++ js/script.js
@@ -7,3 +7,24 @@ let period;
 
 alert('Первый урок какой-то легкий, подозрительно');
 console.log('И сюда напишем что-нибудь');
+
+money = 15000;
+income = 'маникюрчик';
+addExpenses = 'еда, транспорт, интернет, вкусняшки';
+deposit = true;
+mission = 1000000;
+period = 3;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log('Длина строки addExpenses: ', addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log((addExpenses.toLowerCase()).split(', '));
+
+let budgetDay = 'Доход за месяц' + ' / ' + money;
+
+console.log(budgetDay);
